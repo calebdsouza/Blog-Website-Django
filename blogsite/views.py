03103bab57817099aec7f833d5e127c4a0d23e1f
calebdsouza <ca.ds201@gmail.com>
2017-09-07 13:38:34 -0400

    Updated views.py

--- blogsite/views.py
+++ blogsite/views.py
@@ -5,7 +5,8 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blogsite/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     Post.objects.get(pk=pk)
+     return render(request, 'blogsite/post_detail.html', {'post': post})
